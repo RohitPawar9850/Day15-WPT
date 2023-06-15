ec48d04489bf3a9fc6e2f16dbfd2ffd4f1c3eafe
Rohit Pawar <friends9850@gmail.com>
2023-06-15 18:16:34 +0530

    b13

--- src/App.js
+++ src/App.js
@@ -1,61 +1,10 @@
-import { useRef, useState } from "react";
 
-function App() {
-  return (
-    <>
-    <div className="bg-dark-subtle">
-      <h1 className="bg-primary">Stateful List</h1>
-      <ListDemo />
-      </div>
-    </>
+function App()
+{
+  return(
+    
   );
 }
 
-function ListDemo() {
-  let inputRef = useRef();
-  let [list, setList] = useState(["delhi"]);
-
-  let addItemAction = () => {
-    let inputValue = inputRef.current.value;
-    let newList = [inputValue, ...list];
-    setList(newList);
-
-    inputRef.current.value = "";
-  };
-
-  return (
-    <>
-      <input
-        type="text"
-        id="id1"
-        ref={inputRef}
-        placeholder="Enter user input..."
-      />
-      <input type="button" value="Add New Item" onClick={addItemAction} />
-
-      {list.map((item) => (
-        <MessageDemo message={item} />
-      ))}
-    </>
-  );
-}
-
-function MessageDemo({ message }) {
-  return (
-    <>
-      <h1>Hello {message}</h1>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus,
-        dolorem quas sequi tenetur quo odit cupiditate assumenda voluptas
-        quibusdam blanditiis voluptatem quis mollitia eaque molestiae animi,
-        provident eligendi facilis incidunt.
-      </p>
-      <div>
-        <input type="button" value="&#128077;" />
-        <input type="button" value="&#128078;" />
-      </div>
-    </>
-  );
-}
 
 export default App;
