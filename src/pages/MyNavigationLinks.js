ab5a0d97f409632bf6d861f90f3d8a4d79e0c071
Rohit Pawar <friends9850@gmail.com>
2023-06-16 19:45:40 +0530

    validation frontend

--- src/pages/MyNavigationLinks.js
+++ src/pages/MyNavigationLinks.js
@@ -5,7 +5,10 @@ function MyNavigationLinks() {
     <>
       <Link to={"/home"}>Home |</Link>
       <Link to={"/todo"}>Todo |</Link>
-      <Link to={"/registration"}>Registration</Link>
+      <Link to={"/todo-list"}>Todo List |</Link>
+      <Link to={"/registration"}>Registration |</Link>
+      <Link to={"/user-list"}>User List |</Link>
+      <Link to={"/validation-demo"}>ValidationDemo</Link>
     </>
   );
 }
