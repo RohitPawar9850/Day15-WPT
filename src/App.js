ce38f880c3f9095e97cd4114108b559cc9e6b3e0
Rohit Pawar <friends9850@gmail.com>
2023-06-16 09:46:25 +0530

    navigation-links

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import { useRef, useState } from "react";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import MyTodo from "./pages/MyTodo";
 import MyRegistration from "./pages/MyRegistration";
 
 function App() {
   return (
     <>
-    <MyTodo></MyTodo>
-    <br />
-    <hr />
-    <br />
-      <MyRegistration />
+      <BrowserRouter>
+        <Link to={"/home"}>Home |</Link>
+        <Link to={"/todo"}>Todo |</Link>
+        <Link to={"/registration"}>Registration</Link>
+
+        <Routes>
+          {/** 1 Route means 1 Page */}
+          <Route path="/" element={<h1>Hello</h1>} />
+          <Route path="/home" element={<h1>Home</h1>} />
+          <Route path="/todo" element={<MyTodo />} />
+          <Route path="/registration" element={<MyRegistration />} />
+          <Route path="*" element={<h1>Page Not Found</h1>} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
